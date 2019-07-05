cf14996bea80e490e7e5f9e2278f67ef9c8aa5f2
Dennis Chung <chungdennis6@gmail.com>
2019-07-05 14:05:04 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,6 +16,6 @@ end
   get '/friends' do
     # Write your code here!
 @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
-
+erb:friends
   end
 end
